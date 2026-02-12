54d271dc6d76ad848cc740be5ac5a3198fdd8094
Juan Diego Rubiano <jrubianov@unal.edu.co>
2026-02-12 10:30:45 -0500

    modificar archivos de multiplicador, corregir contador

--- calculadora/rtl/cores/Multiplicador/Contador_Multiplicador.v
+++ calculadora/rtl/cores/Multiplicador/Contador_Multiplicador.v
@@ -4,17 +4,17 @@ module contador_Multiplicador
     input   reset,  
     input   in,
   
-    output  reg [15:0] out,
+    output  reg [4:0] out,
     output  zero 
 );
     always @(posedge clk) begin, //logica secuencial en flanco de subida 
 
         if(reset)
-            out <= 0;
+            out <= 5'd16;
         else if(in)
-            out <= out + 1;
+            out <= out - 1;
     end
     
-    assign zero = (out == 16) ? 1 : 0; 
+    assign zero = (out == 0) ? 1 : 0; 
 
 endmodule
