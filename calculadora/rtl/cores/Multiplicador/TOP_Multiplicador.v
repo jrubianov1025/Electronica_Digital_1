8f45238831816061d9d37e6aeb9e9624cbfa26a5
Juan Diego Rubiano <jrubianov@unal.edu.co>
2026-02-12 10:30:45 -0500

    modificar archivos de multiplicador, corregir contador

--- calculadora/rtl/cores/Multiplicador/TOP_Multiplicador.v
+++ calculadora/rtl/cores/Multiplicador/TOP_Multiplicador.v
@@ -16,11 +16,33 @@ wire W_LD;
 wire W_SH;
 wire W_ADD;
 wire W_DEC;
-wire W_Z;
 wire W_LSB;
 
 
+//revisar si esto esta bien echo
 
+Control_Multiplicador Control_Multiplicador0(
+    
+    .clk(clk),
+    .init(init),
+    .Z(zero),
+    .LSB(),
 
+    .ADD(),
+    .SH(),
+    .DEC(),
+    .LD(),
+    .DONE()
+);
+
+contador_Multiplicador contador_Multiplicador0(
+
+    .clk(clk),
+    .reset(LD),
+    .in(DEC),
+
+    .out(),
+    .zero()
+);
 
 endmodule
